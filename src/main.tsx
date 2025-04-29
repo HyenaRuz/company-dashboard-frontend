80918d8134983b8c8629773b0eba7839c576c5fe
HyenaRuz <hyena.ruz@gmail.com>
2025-04-29 18:27:30 +0100

    feat(all): fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import { ThemeProvider, createTheme } from '@mui/material'
+import { ThemeProvider } from '@mui/material'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App.tsx'
@@ -10,15 +10,9 @@ import { theme } from './theme/index.ts'
 
 export const queryClient = new QueryClient()
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-})
-
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme} defaultMode='system'>
+    <ThemeProvider theme={theme} defaultMode="system">
       <Router>
         <App />
       </Router>
